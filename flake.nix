82658b53dbbf6092c7586f898eb02a49029e4643
Jeff Kreeftmeijer <jeff@kreeft.me>
2025-11-22 10:45:17 +0100

    Add flake to run readme.el without cloning the repository
    The flake's default package is a script named readme that takes two
    arguments. The first one is the input file (like org-readme.org), and
    the second one is the output file (like README.md).
    
    To generate a README file from a checkout of readme.el, point to the
    local flake:
    
        nix run . -- org-readme.org README.md
    
    To use readme.el straight from GitHub, point to
    github:jeffkreeftmeijer/readme.el:
    
        nix run github:jeffkreeftmeijer/readme.el -- org-readme.org README.md

--- flake.nix
+++ flake.nix
@@ -12,8 +12,37 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
+
+        readme-el = pkgs.stdenv.mkDerivation {
+          name = "readme.el";
+          src = ./.;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp readme.el $out
+          '';
+        };
+
+        emacs = ((pkgs.emacsPackagesFor pkgs.emacs-nox).emacsWithPackages (epkgs: [
+          epkgs.ox-gfm
+          (epkgs.trivialBuild {
+            pname = "ox-md-title";
+            version = "0.3.0";
+            src = pkgs.fetchFromGitHub {
+              owner = "jeffkreeftmeijer";
+              repo = "ox-md-title.el";
+              rev = "53ddb4c8001ba081eb994c2d9444b5ef860c7e9d";
+              sha256="sha256-kKMaPEw2w52qwyXX4id+PLDxxE0XPq84vr70iKoHUcY=";
+            };
+          })
+        ]));
+
       in {
-        packages.default = pkgs.emacs;
+        packages.default = pkgs.writeShellApplication {
+          name = "readme";
+          text = ''
+            ${emacs}/bin/emacs --no-site-file --batch "$1" --load ${readme-el}/readme.el --eval "(readme/to-markdown \"$2\")"
+          '';
+        };
       }
     );
 }
