ca8c4fec49ad6058cb7da302ed1b3f73ceb8b646
Jeff Kreeftmeijer <jeff@kreeft.me>
2025-11-22 09:38:14 +0100

    Only load vendored dependencies when they're not already present
    Instead of always loading the file before requiring the feature, try
    to require it and load it if it isn't already. This allows readme.el
    to run in emacs versions that already have its requirements installed.

--- readme.el
+++ readme.el
@@ -1,9 +1,10 @@
 (let ((directory (file-name-directory load-file-name)))
-  (load (concat directory "ox-gfm/ox-gfm"))
-  (load (concat directory "ox-md-title/ox-md-title")))
+  (unless (require 'ox-gfm nil t)
+    (require 'ox-gfm (concat directory "ox-gfm/ox-gfm")))
 
-(require 'ox-gfm)
-(require 'ox-md-title)
+  (unless (require 'ox-md-title nil t)
+    (require 'ox-md-title (concat directory "ox-md-title/ox-md-title"))))
+
 (org-md-title-add)
 
 (defun readme/to-markdown (filename)
